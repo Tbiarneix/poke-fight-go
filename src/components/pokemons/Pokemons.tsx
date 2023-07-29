653f554f0add38442eb57dd957f230947089b578
Thomas <thomas.biarneix@gmail.com>
2023-07-30 00:18:22 +0200

    Add filter by name

--- src/components/pokemons/Pokemons.tsx
+++ src/components/pokemons/Pokemons.tsx
@@ -7,7 +7,8 @@ import { Pokemon } from '../../interfaces/pokemonInterfaces'
 import { PokemonCard } from '../pokemonCard/PokemonCard'
 
 const Pokemons = () => {
-  const [pokemonList, setPokemonList] = useState<any>([])
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([])
+  const [filteredPokemonList, setFilteredPokemonList] = useState<Pokemon[]>(pokemonList)
 
   const fetchPokemons = async () => {
     try {
@@ -24,17 +25,37 @@ const Pokemons = () => {
     fetchPokemons()
   }, [])
 
+  const handleFilterChange = (e: any) => {
+    const filterKeyword = e.target.value.toLowerCase()
+    const filteredList = pokemonList.filter((pokemon) =>
+      pokemon.name.fr.toLowerCase().includes(filterKeyword),
+    )
+    setFilteredPokemonList(filteredList)
+  }
+
   return (
     <div>
-      <h2>Pokemons</h2>
+      <h2>Pokemon</h2>
+      <div className="find-pokemon">
+        <h3>Find a Pokemon</h3>
+        <input type="text" placeholder="Search a pokemon" onChange={handleFilterChange} />
+      </div>
       <div className="pokemon-list">
-        {pokemonList.map((pokemon: Pokemon) => {
-          return (
-            <div className="card" key={pokemon.pokedexId}>
-              <PokemonCard {...pokemon} />
-            </div>
-          )
-        })}
+        {filteredPokemonList
+          ? filteredPokemonList.map((pokemon: Pokemon) => {
+              return (
+                <div className="card" key={pokemon.pokedexId}>
+                  <PokemonCard {...pokemon} />
+                </div>
+              )
+            })
+          : pokemonList.map((pokemon: Pokemon) => {
+              return (
+                <div className="card" key={pokemon.pokedexId}>
+                  <PokemonCard {...pokemon} />
+                </div>
+              )
+            })}
       </div>
     </div>
   )
